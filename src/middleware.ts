8794eb1cbdd5345eb0976c064b2b5c5bd6f86c49
devjiwonchoi <devjiwonchoi@gmail.com>
2023-09-13 19:26:47 +0900

    chore: rewrite urls with /docs

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,4 +5,8 @@ export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname === '/getting-started') {
     return NextResponse.redirect(new URL('/', request.url))
   }
+  if (request.nextUrl.pathname.includes('/docs')) {
+    const newURL = request.nextUrl.pathname.replace('/docs', '')
+    return NextResponse.rewrite(new URL(newURL, request.url))
+  }
 }
