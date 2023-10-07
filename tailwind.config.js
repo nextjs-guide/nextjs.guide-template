4f3573cc532bebdd5b351b53a80eb3437144d672
devjiwonchoi <devjiwonchoi@gmail.com>
2023-10-07 14:21:18 +0900

    create next app

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: ['class', 'html[class~="dark"]'],
-  content: [
-    './src/components/**/*.js',
-    './src/pages/**/*.{md,mdx}',
-    './theme.config.tsx',
-  ],
-}
